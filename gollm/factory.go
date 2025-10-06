544cdbd0a25491639ac9a327c2d49e7370d0b903
Noah West <westnoah@google.com>
2025-10-06 10:54:04 -0700

    Revert "set http timeout from 60 -> 180s to be kinder to local llm stacks"
    This reverts commit 035e9473d7e85510fd85b6087b19c9b8364ca348.

--- gollm/factory.go
+++ gollm/factory.go
@@ -193,12 +193,19 @@ func DefaultIsRetryableError(err error) bool {
 // createCustomHTTPClient returns an *http.Client that optionally skips SSL certificate verification.
 // This is shared by all providers that need custom HTTP transport.
 func createCustomHTTPClient(skipVerify bool) *http.Client {
-	transport := http.DefaultTransport.(*http.Transport).Clone()
-	transport.TLSClientConfig = &tls.Config{InsecureSkipVerify: skipVerify}
-
+	if !skipVerify {
+		httpClient := http.DefaultClient
+		httpClient.Timeout = 60 * time.Second
+		return httpClient
+	}
 	return &http.Client{
-		Transport: transport,
-		Timeout:   180 * time.Second,
+		Transport: &http.Transport{
+			Proxy: http.ProxyFromEnvironment,
+			TLSClientConfig: &tls.Config{
+				InsecureSkipVerify: true,
+			},
+		},
+		Timeout: 60 * time.Second,
 	}
 }
 
