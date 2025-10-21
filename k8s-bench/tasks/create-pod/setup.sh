b65923f4b077924f2024396425ea7c0c3f9ceb8b
Noah West <westnoah@google.com>
2025-10-21 14:11:46 -0700

    Update create-pod ns to avoid hinting, check that image is correct

--- k8s-bench/tasks/create-pod/setup.sh
+++ k8s-bench/tasks/create-pod/setup.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-kubectl delete namespace create-pod-test --ignore-not-found
-kubectl create namespace create-pod-test
+NAMESPACE="web-server"
+kubectl delete namespace $NAMESPACE --ignore-not-found
+kubectl create namespace $NAMESPACE
